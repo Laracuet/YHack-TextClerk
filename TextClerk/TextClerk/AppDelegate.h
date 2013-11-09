1a13dfad1cafaf2ebe7718fd7314236dbe5607f9
KFCharron <charronkyle@gmail.com>
2013-11-09 00:35:04 -0500

    First Commit

---
+++ TextClerk/TextClerk/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextClerk
+//
+//  Created by Kyle Charron on 11/9/13.
+//  Copyright (c) 2013 Kyle Charron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TextClerk/TextClerk/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TextClerk
+//
+//  Created by Kyle Charron on 11/9/13.
+//  Copyright (c) 2013 Kyle Charron. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
